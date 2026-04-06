5defc21aaebcf412bcb4af31c06d933a7b948853
heisenberg-c1 <3151765378@qq.com>
2026-04-06 11:39:58 +0800

    feat: 项目初始化

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+const root = path.dirname(fileURLToPath(import.meta.url))
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(root, './src'),
+    },
+  },
+})
